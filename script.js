5e6dc5be814806c9e381da7291e60cd7ecd1a918
dilanr <dilanespinoza38@gmail.com>
2025-05-17 15:39:56 -0400

    Style-estilos implementados en proyecto.

--- script.js
+++ script.js
@@ -6,44 +6,59 @@ const averageDiv=document.getElementById("average");
 document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
 
-    const name=document.getElementById("name").value.trim();
-    const lastName=document.getElementById("lastName").value.trim();
-    const grade=parseFloat(document.getElementById("grade").value.trim());
 
-    if(grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
-    alert("Error Datos Incorrectos")
-    return
+    const name = document.getElementById("name").value.trim();
+
+    const lastName = document.getElementById("lastname").value.trim();
+
+    const grade = document.getElementById("grade").value.trim();
+
+    const date = document.getElementById("date").value.trim();
+
+    if(grade <1 || grade>7 || !name || !lastName || isNaN(grade) ){
+        alert("Error Datos Incorrectos")
+        return
     }
 
     //guardar datos en el Array nuevo
 
-    const student={name,lastName,grade};
+    const student ={name,lastName,grade: parseFloat(grade),date};
     students.push(student);
      addStudentToTable(student)
-     calcularPromedio()
    // console.log(students)
 
     this.reset();
+    calcularPromedio()
 
 });
 
 function addStudentToTable(student){
     const row=document.createElement("tr");
     row.innerHTML=
-    `<td>${student.name}</td>
-     <td>${student.lastName}</td>
-    <td>${student.grade}</td>`;
-   tableBody.appendChild(row);
+    `<td>${student.name} </td>
+    <td>${student.lastName} </td>
+    <td>${student.grade} </td>
+    <td>${student.date} </td>`
+    ;
+    tableBody.appendChild(row);
 }
 
 function calcularPromedio(){
-    if(students.length===0){
-       averageDiv.textContent="Promedio General del Curso:N/A"
+    if (students.length ===0){
+        averageDiv.textContent="Promedio de Calificaciones: No Disponible";
+        averageDiv.style.color = "black";
         return;
     }
-    const total=students.reduce((sum,s)=>sum+s.grade,0);
-    console.log(total)
-    const average=total/students.length;
-    console.log(average)
-    averageDiv.textContent=`Promedio General del Curso: ${average.toFixed(2)}`;
+
+    const notas = students.map(estudiante => estudiante.grade)
+    const suma = notas.reduce((acumulador, valorActual) => acumulador + valorActual,0);
+
+    let promedio = suma/students.length
+    averageDiv.textContent=`Promedio de Calificaciones: ${promedio.toFixed(2)}`;
+
+    if (promedio >= 4){
+        averageDiv.style.color = "green";
+    } else {
+        averageDiv.style.color = "red";
+    }
 }
